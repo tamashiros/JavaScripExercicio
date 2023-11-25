689178ee9e30eb1d564e214af6945b324b499dd6
Keiichi Tamashiro <127620213+tamashiros@users.noreply.github.com>
2023-11-25 09:36:27 -0300

    Adcionando exercios do sabado passado
    atualizando os exercicios

---
+++ DesafioArea.js
@@ -0,0 +1,7 @@
+// entrada de dados
+ let Pi =3.14;
+ let raio = 16;
+ //prcessamento 
+ let area = Pi*raio*raio;
+ //resultado
+ console.log( ' Aarea da circuferencia é : ' + area);
---
+++ script01.js
@@ -0,0 +1,23 @@
+//trabalhando com variaveis
+//utizando padrão CamelCase
+// variavel (let,var,const)-> identificador -> dados
+
+const carro ="carro"
+const fabricante_carro ="reunault"
+const modeloCarro = "sandero"
+const corDeCarro = "vermelho"
+const anoCarro = 2010
+//saida de dados
+// Carro : "carro"
+//fabricanteCarro: "reunalt"
+// cor De carro : "vermelho"
+// anoCarro : anoCarro
+//console.log("carro é " + carro + "fabricante " + fabricante_carro, "modelo do carro é " + modeloCarro
+//,"cor de carro " + corDeCarro, "ano " + anoCarro);
+// segunda forma de interpolação
+console.log('meu carro:${carro}, fabricante do carro: $ {fabricante_Carro}, modelo carro : ${modeloCarro}, cor do carro : $ { corDeCarro}, ano do carro: $ {anoCarro}');
+
+//console.log(carro);
+//console.log(fabricante_carro);
+//console.log(modeloCarro);
+//console.log(anoCarro);
